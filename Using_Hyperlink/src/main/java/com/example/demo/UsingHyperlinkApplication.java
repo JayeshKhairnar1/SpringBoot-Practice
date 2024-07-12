45c8d8e9d7c25b677b088f3739e0853c91228f04
Jayesh Khairnar <jayeshkhairnar6@gmail.com>
2024-07-12 13:40:47 +0530

    adding introductory projects

---
+++ Using_Hyperlink/src/main/java/com/example/demo/UsingHyperlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingHyperlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingHyperlinkApplication.class, args);
+	}
+
+}
